3be1e667f73e132c3319a7b6a723dd1a9d6038bc
corradoballabio <corradoballabio11@gmail.com>
2023-02-25 17:39:41 +0100

    [board] reset timer on new game

--- src/components/ControlBoard.js
+++ src/components/ControlBoard.js
@@ -10,12 +10,17 @@ function ControlBoard(props) {
     return () => clearInterval(interval);
   }, [props.timerActive]);
 
+  const handleNewGameClick = () => {
+    setTimer(0)
+    props.onNewGameClick()
+  }
+
   return(
     <div className='header'>
       <h2>minesweeper</h2>
       <ul>
         <li>{timer}</li>
-        <li><button onClick={props.onNewGameClick}>start</button></li>
+        <li><button onClick={handleNewGameClick}>start</button></li>
         <li><button onClick={props.onFlagClick}>flag</button></li>
       </ul>
     </div>
